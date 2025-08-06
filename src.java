727f5d9f83430062f55fa8744c439a6cd022afe5
yamini43 <yaminiguduri56@gmail.com>
2025-08-06 17:20:55 +0530

    Delete src.java

--- src.java
+++
@@ -1,9 +0,0 @@
-public class Addition {
-    public static void main(String[] args) {
-        int a = 10;
-        int b = 20;
-        int sum = a + b;
-
-        System.out.println("The sum of " + a + " and " + b + " is: " + sum);
-    }
-}
